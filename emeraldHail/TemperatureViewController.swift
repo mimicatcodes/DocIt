e4e21592350142be9fa849122be8cc504f43359b
Taniraw <tanirawrites@gmail.com>
2016-11-23 21:11:20 -0500

    added-tempererature-view

---
+++ emeraldHail/TemperatureViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TemperatureViewController.swift
+//  emeraldHail
+//
+//  Created by Tanira Wiggins on 11/23/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+
+    // Tanira: This data is to be used later with the beta version of the application. Please do not remove commented out section below. Thank you :D
+    // 0-2 years old rectal thermometer normal is 97.9-100.4 ->Armpit 94.5-99.1 -> Ear 97.5
+    // 3-10 years oral thermometer normal is 95.9-99.5 -> Rectal 97.9-100.4-> Armpit 96.6-98.0 -> Ear 97.0-100.0
+    // Over age 11 oral thermometer normal is 97.6-99.6 -> rectal 98.6-100.6 -> Armpit 95.3-98.4 -> Ear 96.6-99.7
+    
+ //   var availableTemps: [Int] = [96.0, 96.2, 96.4, 96.8, 97.0, ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
